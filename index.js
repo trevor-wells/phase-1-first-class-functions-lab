22b11d0d333cee40ee740880de8f6ef4f7d320b7
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-19 18:28:21 -0600

    Final commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = names => [names[0] , names[1]];
+
+const returnLastTwoDrivers = names => [names[names.length - 2] , names[names.length - 1]];
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return fare => fare * multiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(names , selection) {
+    return selection(names);
+}
